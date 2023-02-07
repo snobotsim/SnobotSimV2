e2551f38fe3578727acc0b5c9cbacb1ea59a2e4b
PJ Reiniger <pj.reiniger@gmail.com>
2023-02-07 16:59:44 -0500

    Add pigeon2 sim (#29)
    * Add pigeon2 simulator
    
    * lint
    
    * lint

--- snobot_sim_java/src/main/java/org/snobotv2/module_wrappers/ctre/CtrePigeonImuWrapper.java
+++ snobot_sim_java/src/main/java/org/snobotv2/module_wrappers/ctre/CtrePigeonImuWrapper.java
@@ -1,11 +1,17 @@
 package org.snobotv2.module_wrappers.ctre;
 
+import com.ctre.phoenix.sensors.WPI_Pigeon2;
 import com.ctre.phoenix.sensors.WPI_PigeonIMU;
 import org.snobotv2.module_wrappers.BaseGyroWrapper;
 
 public class CtrePigeonImuWrapper extends BaseGyroWrapper
 {
     public CtrePigeonImuWrapper(WPI_PigeonIMU pigeon)
+    {
+        super((double angle) -> pigeon.getSimCollection().setRawHeading(-angle));
+    }
+
+    public CtrePigeonImuWrapper(WPI_Pigeon2 pigeon)
     {
         super((double angle) -> pigeon.getSimCollection().setRawHeading(-angle));
     }
