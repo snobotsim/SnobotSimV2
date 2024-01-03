dd2a6ce458fe642d1adb4921fce7769e8eecd4f9
PJ Reiniger <pj.reiniger@gmail.com>
2024-01-03 18:28:37 -0500

    Add phoenix6 pigeon simulator

---
+++ snobot_sim_java_phoenix6/src/test/java/org/snobotv2/module_wrappers/phoenix6/BasePhoenix6UnitTest.java
@@ -0,0 +1,21 @@
+package org.snobotv2.module_wrappers.phoenix6;
+
+import edu.wpi.first.hal.HAL;
+import org.snobotv2.test_utils.BaseUnitTest;
+
+public class BasePhoenix6UnitTest extends BaseUnitTest
+{
+    protected void ctreSimLoop()
+    {
+        HAL.simPeriodicBefore();
+        HAL.simPeriodicAfter();
+
+        try
+        {
+            Thread.sleep(20);
+        } catch (InterruptedException e)
+        {
+            e.printStackTrace(); // NOPMD
+        }
+    }
+}
